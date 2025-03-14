8df0a6578a4068c44ea437f629f6c7885af8c8e0
LUCAS_OLIVEIRA_DIAS <diasfpshs@gmail.com>
2025-03-14 08:26:05 -0300

    Novo Css Tailwind Login

--- sistema/index.php
+++ sistema/index.php
@@ -45,7 +45,7 @@ if ($total_reg == 0) {
             <div>
                 <input type="password" name="senha" placeholder="Senha" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
             </div>
-            <button type="submit" class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">LoginNNNNN</button>
+            <button type="submit" class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">LoginDDAS</button>
         </form>
 
         <p class="text-center text-sm mt-4">
