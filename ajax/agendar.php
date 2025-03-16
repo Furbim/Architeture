914afce987c8024571e6dced81aa0e39cac71291
Thiago Martins de Freitas <thiagofreitasofc202@gmail.com>
2025-03-15 21:19:27 -0300

    Consertando o sistema financeiro, parte do cliente

--- ajax/agendar.php
+++ ajax/agendar.php
@@ -1,20 +1,51 @@
 <?php
 require_once("../sistema/conexao.php");
 
-$telefone = $_POST['telefone'];
-$nome = $_POST['nome'];
-$funcionario = $_POST['funcionario'];
+date_default_timezone_set('America/Sao_Paulo'); // Garante que o fuso horário está correto
+
+// Verifica se a conexão está ativa
+if (!$pdo) {
+    die("Erro na conexão com o banco de dados!");
+}
+
+$telefone = $_POST['telefone'] ?? '';
+$nome = $_POST['nome'] ?? '';
+$funcionario = $_POST['funcionario'] ?? '';
 $hora = $_POST['hora'] ?? '';
-$servico = $_POST['servico'];
-$obs = $_POST['obs'];
-$data = $_POST['data'];
+$servico = $_POST['servico'] ?? '';
+$obs = $_POST['obs'] ?? '';
+$data = $_POST['data'] ?? '';
 $id = $_POST['id'] ?? '';
 
-// Se não houver horário, interrompe a execução
-if (empty($hora)) {
-    echo 'Escolha um Horário para Agendar!';
+if (empty($data) || empty($hora)) {
+    echo 'Data e horário são obrigatórios!';
     exit();
 }
+
+if (strpos($data, '/') !== false) {
+    $dataFormatada = DateTime::createFromFormat('d/m/Y', $data);
+    if ($dataFormatada) {
+        $data = $dataFormatada->format('Y-m-d');
+    } else {
+        echo 'Formato de data inválido!';
+        exit();
+    }
+}
+
+$hora = trim($hora);
+if (strlen($hora) == 5) { 
+    $hora .= ':00';
+}
+
+$dataHoraAgendamento = "$data $hora";
+$timestampAtual = strtotime(date('Y-m-d H:i:s'));
+$timestampAgendamento = strtotime($dataHoraAgendamento);
+
+if ($timestampAgendamento < $timestampAtual) {
+    echo 'Não é possível agendar para um horário que já passou!';
+    exit();
+}
+
 
 // Verificar se o horário já está ocupado
 $query = $pdo->prepare("SELECT * FROM agendamentos WHERE data = :data AND hora = :hora AND funcionario = :funcionario");
@@ -34,10 +65,8 @@ $res = $query->fetchAll(PDO::FETCH_ASSOC);
 if (count($res) > 0) {
     $id_cliente = $res[0]['id'];
 } else {
-    // Caso o cliente não exista, cadastrá-lo automaticamente
-    $query = $pdo->prepare("INSERT INTO clientes (nome, telefone) VALUES (:nome, :telefone)");
-    $query->execute(['nome' => $nome, 'telefone' => $telefone]);
-    $id_cliente = $pdo->lastInsertId();
+    // Caso o cliente não exista, retornar a página de login
+    header('Location: agendamentos.php');
 }
 
 if (empty($id)) {
--- ajax/listar-horarios.php
+++ ajax/listar-horarios.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once("../sistema/conexao.php");
 @session_start();
 $usuario = @$_SESSION['id'];
@@ -7,72 +7,71 @@ $funcionario = @$_POST['funcionario'];
 $data = @$_POST['data'];
 $hora_rec = @$_POST['hora'];
 
-
-
-if($funcionario == ""){
-	
+// Verificar se o funcionário foi informado
+if (empty($funcionario)) {
+	echo 'Funcionário não informado!';
 	exit();
 }
 
+// Verificar se a data foi informada
+if (empty($data)) {
+	echo 'Data não informada!';
+	exit();
+}
 
-$diasemana = array("Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sabado");
+// Verificar o dia da semana da data informada
+$diasemana = array("Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado");
 $diasemana_numero = date('w', strtotime($data));
 $dia_procurado = $diasemana[$diasemana_numero];
 
-//percorrer os dias da semana que ele trabalha
-$query = $pdo->query("SELECT * FROM dias where funcionario = '$funcionario' and dia = '$dia_procurado'");
+// Verificar se o funcionário trabalha no dia informado
+$query = $pdo->prepare("SELECT * FROM dias WHERE funcionario = :funcionario AND dia = :dia");
+$query->execute(['funcionario' => $funcionario, 'dia' => $dia_procurado]);
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
-if(@count($res) == 0){
-		echo 'Este Funcionário não trabalha neste Dia!';
+
+if (count($res) == 0) {
+	echo 'Este Funcionário não trabalha neste Dia!';
 	exit();
 }
 
+// Buscar os horários disponíveis para o funcionário
+$query = $pdo->prepare("SELECT * FROM horarios WHERE funcionario = :funcionario ORDER BY horario ASC");
+$query->execute(['funcionario' => $funcionario]);
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+
+if (count($res) == 0) {
+	echo 'Nenhum horário disponível para este funcionário!';
+	exit();
+}
 ?>
+
 <div class="row">
+	<?php
+	foreach ($res as $horario) {
+		$hora = $horario['horario'];
+		$horaF = date("H:i", strtotime($hora));
 
-	<?php 
-	$query = $pdo->query("SELECT * FROM horarios where funcionario = '$funcionario' ORDER BY horario asc");
-	$res = $query->fetchAll(PDO::FETCH_ASSOC);
-	$total_reg = @count($res);
-	if($total_reg > 0){
-		for($i=0; $i < $total_reg; $i++){
-			foreach ($res[$i] as $key => $value){}
-				$hora = $res[$i]['horario'];
-				$horaF = date("H:i", strtotime($hora));
+		// Verificar se o horário já está agendado
+		$query2 = $pdo->prepare("SELECT * FROM agendamentos WHERE data = :data AND hora = :hora AND funcionario = :funcionario");
+		$query2->execute(['data' => $data, 'hora' => $hora, 'funcionario' => $funcionario]);
+		$res2 = $query2->fetchAll(PDO::FETCH_ASSOC);
 
+		// Se o horário não estiver agendado ou for o horário selecionado, exibir o radio button
+		if (count($res2) == 0 || $hora_rec == $hora) {
+			$checado = ($hora_rec == $hora) ? 'checked' : '';
+			?>
+			<div class="col-3">
+				<div class="form-check">
+					<input class="form-check-input" type="radio" name="hora" value="<?php echo $hora ?>" <?php echo $checado ?>
+						style="width:17px; height: 17px" required>
+					<label class="form-check-label" for="flexRadioDefault1">
+						<?php echo $horaF ?>
+					</label>
+				</div>
+			</div>
+			<?php
 
-				//validar horario
-$query2 = $pdo->query("SELECT * FROM agendamentos where data = '$data' and hora = '$hora' and funcionario = '$funcionario'");
-$res2 = $query2->fetchAll(PDO::FETCH_ASSOC);
-$total_reg2 = @count($res2);
-if($total_reg2 == 0 || strtotime($hora_rec) == strtotime($hora)){
-	$hora_agendada = '';
-	$texto_hora = '';
-
-	if(strtotime($hora_rec) == strtotime($hora)){
-		$checado = 'checked';
-	}else{
-		$checado = '';
-	}
-
-				?>
-
-				<div class="col-3">
-					<div class="form-check">
-					  <input class="form-check-input" type="radio" name="hora" value="<?php echo $hora ?>" <?php echo $hora_agendada ?> style="width:17px; height: 17px" required <?php echo $checado ?>>
-					  <label class="form-check-label <?php echo $texto_hora ?>" for="flexRadioDefault1">
-					    <?php echo $horaF ?>
-					  </label>
-					</div>
-				</div> 
-
-				<?php 
-				
 		}
-
-	}
 	}
 	?>
-
-
 </div>
