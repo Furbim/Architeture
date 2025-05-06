b72f06cdaa8bbe243e047eaa98d04914bccf6728
Thiago Martins de Freitas <thiagofreitasofc202@gmail.com>
2025-05-06 13:57:03 -0300

    fix: bug de dias e horarios duplicados

--- sistema/painel/paginas/funcionarios/inserir-dias.php
+++ sistema/painel/paginas/funcionarios/inserir-dias.php
@@ -5,9 +5,22 @@ $tabela = 'dias';
 $id = $_POST['id'];
 $dias = $_POST['dias'];
 
+// Verificar se o dia já está cadastrado para o funcionário
+$query = $pdo->prepare("SELECT * FROM $tabela WHERE funcionario = :id AND dia = :dias");
+$query->bindParam(':id', $id);
+$query->bindParam(':dias', $dias);
+$query->execute();
 
-$pdo->query("INSERT INTO $tabela SET dia = '$dias', funcionario = '$id'");
+if ($query->rowCount() > 0) {
+    echo 'Dia já cadastrado!';
+    exit();
+}
 
+// Inserir novo dia
+$query = $pdo->prepare("INSERT INTO $tabela (dia, funcionario) VALUES (:dias, :id)");
+$query->bindParam(':dias', $dias);
+$query->bindParam(':id', $id);
+$query->execute();
 
 echo 'Salvo com Sucesso';
- ?>
+?>
