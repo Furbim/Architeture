d48a8a9d8aa26d7606f7657de455780146b537ea
Thiago Martins de Freitas <thiagofreitasofc202@gmail.com>
2025-05-06 14:00:28 -0300

    fix: bug de dias e horarios duplicados

--- sistema/painel/paginas/funcionarios/inserir-dias.php
+++ sistema/painel/paginas/funcionarios/inserir-dias.php
@@ -5,6 +5,23 @@ $tabela = 'dias';
 $id = $_POST['id'];
 $dias = $_POST['dias'];
 
+// Lista de dias válidos da semana
+$dias_validos = [
+    'Segunda-Feira',
+    'Terça-Feira',
+    'Quarta-Feira',
+    'Quinta-Feira',
+    'Sexta-Feira',
+    'Sábado',
+    'Domingo'
+];
+
+// Verificar se o valor enviado é um dia válido
+if (!in_array($dias, $dias_validos)) {
+    echo 'Dia inválido!';
+    exit();
+}
+
 // Verificar se o dia já está cadastrado para o funcionário
 $query = $pdo->prepare("SELECT * FROM $tabela WHERE funcionario = :id AND dia = :dias");
 $query->bindParam(':id', $id);
